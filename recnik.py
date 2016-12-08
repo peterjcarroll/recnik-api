76392936f9b6cf434f9c50be88f4156a73cc3f50
PeterCarroll <peter.carroll@creativaconsulting.com>
2016-12-08 01:08:41 -0600

    Added lookup of words through Wiktionary

--- recnik.py
+++ recnik.py
@@ -1,5 +1,6 @@
 import hug
 import spell
+from wiktionaryparser import WiktionaryParser
 
 def cors_support(response, *args, **kwargs):
     response.set_header('Access-Control-Allow-Origin', '*')
@@ -7,5 +8,10 @@ def cors_support(response, *args, **kwargs):
 @hug.get(requires=cors_support)
 def suggest(search: hug.types.text):
     words = spell.candidates(search)
-    is_word = len(spell.known([search]))>0
+    is_word = len(spell.known([search])) > 0
     return {'is_word': is_word, 'words': words}
+
+@hug.get(requires=cors_support)
+def lookup(word: hug.types.text): 
+    parser = WiktionaryParser()
+    return parser.fetch(word, 'serbo-croatian')
