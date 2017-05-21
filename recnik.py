0b8f6f2cade0b6dcffa1aed71457a6ced674e97f
PeterCarroll <peter.carroll@creativaconsulting.com>
2017-05-21 15:46:07 -0500

    replaced WiktionaryParser with yawp

--- recnik.py
+++ recnik.py
@@ -1,6 +1,6 @@
 import hug
 import spell
-from wiktionaryparser import WiktionaryParser
+from yawp.dict import serbocroatian
 
 def cors_support(response, *args, **kwargs):
     response.set_header('Access-Control-Allow-Origin', '*')
@@ -11,7 +11,6 @@ def suggest(search: hug.types.text):
     is_word = len(spell.known([search])) > 0
     return {'is_word': is_word, 'words': words}
 
-@hug.get(requires=cors_support)
+@hug.get(requires=cors_support, output=hug.output_format.text)
 def lookup(word: hug.types.text): 
-    parser = WiktionaryParser()
-    return parser.fetch(word, 'serbo-croatian')
+    return serbocroatian.get_json(word)
